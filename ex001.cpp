5dcbd02569d5a560a3ee1e3658b274a2f65b2280
SharkboyHL <104568918+SharkboyHL@users.noreply.github.com>
2024-04-04 23:40:42 +0000

    ...

---
+++ ex001.cpp
@@ -0,0 +1,22 @@
+/* Determinar o tipo de voto de um indivíduo de acordo com o sistema de votação
+brasileiro. Ele é realizado de acordo com a idade das pessoas. As regras de votação
+são:  a) Menores de 16 anos não podem votar; b) De 16 anos até 17 anos, o voto é
+facultativo; c) De 18 anos até 69 anos, o voto é obrigatório; d) Maiores que 70 anos, o
+voto é facultativo. */ 
+
+#include <iostream>;
+
+using namespace std;
+
+int main(){
+
+    cout << "Bem vindo ! Este é o sistema eleitoral"
+
+    int idade;
+    cout << "Insira a sua idade: ";
+    cin >> idade;
+
+
+
+    return 0;
+}
