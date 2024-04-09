3d26ceeac9c6ce6fda304d2541d0276f6b8ce7c8
SharkboyHL <104568918+SharkboyHL@users.noreply.github.com>
2024-04-09 13:04:29 +0000

    Ex005 concluído com sucesso!

--- ex005.cpp
+++ ex005.cpp
@@ -24,8 +24,25 @@ int main(){
     
     float imc = peso / (altura * altura);
 
-    if(imc <=)    
+    //cout << imc << " Kg/m²" << endl;
+
+    if(imc < 18.5){
+        cout << "Seu imc é " << imc << " Kg/m² e sua classificação é Abaixo do Peso." << endl;
+    }else if(imc >= 18.5 && imc <= 24.9){
+        cout << "Seu imc é " << imc << " Kg/m² e sua classificação é Peso Normal." << endl;
+    }else if(imc >= 25 && imc <= 29.9){
+        cout << "Seu imc é " << imc << " Kg/m² e sua classificação é Sobrepeso." << endl;
+    }else if(imc >= 30 && imc <= 34.9){
+        cout << "Seu imc é " << imc << " Kg/m² e sua classificação é Obesidade Grau I." << endl;
+    }else if(imc >= 35 && imc <= 39.9){
+        cout << "Seu imc é " << imc << " Kg/m² e sua classificação é Obesidade Grau II." << endl;
+    }else if(imc >= 40 && imc <= 49.90){
+        cout << "Seu imc é " << imc << " Kg/m² e sua classificação é Obesidade Grau III." << endl;
+    }else if(imc >= 50 && imc <= 59.9){
+        cout << "Seu imc é " << imc << " Kg/m² e sua clasificação é Obesidade Grau IV." << endl;
+    }else{
+        cout << "Seu imc é " << imc << " Kg/m² e sua classificação é Obesidade Grau V." << endl;
+    }
 
     return 0;
 }
-
