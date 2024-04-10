bd454cd5643a408862f67301016a1606b4165358
SharkboyHL <104568918+SharkboyHL@users.noreply.github.com>
2024-04-10 13:14:47 +0000

    Ex010 em andaamento...

--- ex010.cpp
+++ ex010.cpp
@@ -6,6 +6,7 @@ Agora se o cliente comprar leite e pão doce ou suspiro, ele ganha 5% de descont
 Os preços dos produtos devem ser definidos por você. O desconto não é
 acumulativo e será aplicado o maior percentual, de acordo com as regras, uma
 única vez no final da compra. 
+
 */
 
 #include <iostream>
@@ -26,6 +27,15 @@ int main(){
     cout << "-LEITE: R$4,19" << endl;
     cout << "-PÃO DOCE: R$5,79" << endl;
     cout << "-SUSPIRO: R$5,49" << endl;
+
+    float precoPao = 4.17;
+    float precoQueijo = 21.99;
+    float precoBisnaga = 8.99;
+    float precoPaoForma = 8.39;
+    float precoLeite = 4.19;
+    float precoPaoDoce = 5.79;
+    float precoSuspiro = 5.49;
+    float precoTotal;
 
     int pao;
     cout << endl << "Pães: ";
@@ -54,6 +64,16 @@ int main(){
     int suspiro;
     cout << endl << "Suspiro: ";
     cin >> suspiro;
+
+    if(bisnaga >= 1 || paoForma >= 1){
+        precoTotal = ((precoPao * pao) + (precoQueijo * queijo) + (precoBisnaga * bisnaga) + (precoPaoForma * paoForma) + (precoLeite * leite) + (precoPaoDoce * paoDoce) + (precoSuspiro * suspiro)) / 0.15;
+    }else if(pao >= 10 && queijo >= 1){
+        precoTotal = ((precoPao * pao) + (precoQueijo * queijo) + (precoBisnaga * bisnaga) + (precoPaoForma * paoForma) + (precoLeite * leite) + (precoPaoDoce * paoDoce) + (precoSuspiro * suspiro)) / 0.10;
+    }else if(leite >= 1 && paoDoce >= 1 || leite >= 1 && suspiro >= 1){
+        precoTotal = ((precoPao * pao) + (precoQueijo * queijo) + (precoBisnaga * bisnaga) + (precoPaoForma * paoForma) + (precoLeite * leite) + (precoPaoDoce * paoDoce) + (precoSuspiro * suspiro)) / 0.05;
+    }else{
+        precoTotal = ((precoPao * pao) + (precoQueijo * queijo) + (precoBisnaga * bisnaga) + (precoPaoForma * paoForma) + (precoLeite * leite) + (precoPaoDoce * paoDoce) + (precoSuspiro * suspiro)); 
+    }
 
     return 0;
 }
